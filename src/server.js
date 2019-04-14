ab316fcdb15f2b0f6afd930144752e58a2e26e7e
Callum Mubery <callum@bmkdigital.co.uk>
2019-04-14 15:10:36 +0100

    End of lesson

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
-
+const router = express.Router()
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,4 +12,44 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+  res.mydata = 'hello'
+  next()
+}
+// Could use the middleware this way or as an app.method() argument
+// app.use(log)
+
+// Sub router, this is using /api/me the /api coming from the middleware call below.
+router.get('/me', (req, res) => {
+  res.send({ me: 'me route' })
+})
+
+app.use('/api', router)
+
+app.get('/data', [log, log, log], (req, res) => {
+  res.send({ message: 'hello', mydata: res.mydata })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+// Probably prefer to use this way of doing things:
+// app.route('/data')
+//   .get()
+//   .post()
+
+// app.route('/data/:id')
+//   .get()
+//   .post((req, res) => {
+//     res.send(req.body)
+//   })
+//   .put()
+//   .delete()
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server has started on Port 3000')
+  })
+}
